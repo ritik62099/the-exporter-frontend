367b8a852ecef1e237cef66c2a4e945aa73d039e
ritik62099 <ritik841207@gmail.com>
2025-05-01 13:21:28 +0530

    update

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -1,41 +1,4 @@
-// // src/auth/AuthContext.js
 
-// import React, { createContext, useContext, useState, useEffect } from 'react';
-
-// const AuthContext = createContext();
-
-// export const AuthProvider = ({ children }) => {
-//   const [user, setUser] = useState(null);
-
-//   // Check for token on mount
-//   useEffect(() => {
-//     const storedUser = localStorage.getItem("user");
-//     if (storedUser) {
-//       setUser(JSON.parse(storedUser));
-//     }
-//   }, []);
-
-//   const login = (userData) => {
-//     setUser(userData);
-//     localStorage.setItem("user", JSON.stringify(userData));
-//   };
-
-//   const logout = () => {
-//     setUser(null);
-//     localStorage.removeItem("user");
-//     localStorage.removeItem("token");
-//   };
-
-//   return (
-//     <AuthContext.Provider value={{ user, login, logout }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// export const useAuth = () => useContext(AuthContext);
-
-// src/auth/AuthContext.js
 
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
